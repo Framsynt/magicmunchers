57c0f78f832454cd2c2e235dc4449bc9d5b91492
Framsynt <virtuousno1@protonmail.com>
2022-02-04 20:36:20 +0100

    Basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>test</p>
-
+      <header className="section section--title">
+        <div className="content">
+          <h1 className="logo">
+            <span className="logo_welcome">Welcome to</span>
+            <br />
+            Magic Munchers
+          </h1>
+          <p className="subtitle">
+            We're a bunch of furry little monsters roaming the metaverse
+            devouring jpegs and harvesting $MAGIC.
+          </p>
+        </div>
       </header>
+      <div className="section">
+        <div className="content">
+          <p>
+            We’re looking to consume and grow Legions, Treasures, and $MAGIC
+            within Bridgeworld, and other ETH blue-chip, yield-bearing assets as
+            voted upon by the Munchers.
+          </p>
+          <p>
+            Magic Munchers receive $MUNCH tokens representing the fractional
+            shares of the DAO vault. Munchers also get exclusive access within
+            the Magic Munchers Community and voting rights over the DAO's
+            assets. More details to be revealed soon.
+          </p>
+        </div>
+      </div>
+      <div className="section">
+        <div className="content">
+          <p className="caption--mini">
+            Terms & Conditions: You Own Your NFT: By connecting your Ethereum
+            wallet and minting a NFT with our smart contract, you gain full and
+            complete ownership of your NFT. Limitations of Liability for Gas,
+            Failed Transactions, and other Bugs. By minting or purchasing a
+            Magic Munchers NFT or the MUNCH token, you agree to not hold NFTs
+            accountable for any losses you may incur as a consequence of minting
+            your NFT. These potential losses include any gas fees for failed
+            transactions, and any excessive gas fees during the minting process.
+            NFTs Are Not Intended as Investments: NFTs serve exclusively as
+            Non-Fungible Tokens for you to collect. They are not intended for
+            any investment purposes. We make absolutely no promise or guarantee
+            that these NFTs will hold any particular value once minted on our
+            website. You are responsible for any tax liability which may arise
+            from minting or reselling your NFT(s). Class Action Waiver: You
+            agree to waive any class action status, and any legal dispute around
+            the NFTs project. Children: You agree that you are over the age of
+            18, or the legal age within your jurisdiction. Arbitration: In the
+            event that a legal dispute arises from anything related to the NFTs
+            project, you agree to bring the case to binding arbitration
+            according to appropriate Federal guidelines. Jurisdiction and Choice
+            of Law: You agree that for purposes of any legal dispute, you will
+            be subject to the jurisdiction of the United States and that any
+            legal proceeding will be brought in the United States.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
